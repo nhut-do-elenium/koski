8af3f3d8c8cdf39d42f0e83d3ba343e6e6abe0ac
NhutDo <nhut.do@elenium.fi>
2025-01-28 13:51:56 +0200

    OPHJOD-1217 Add ignore CORS for development.

--- omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/config/SecurityConfig.java
+++ omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package fi.opetushallitus.koski.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -29,6 +30,12 @@ public class SecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .oauth2Client(withDefaults()) //Trigger login with /oauth2/authorization/koski
+                .csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection (Only for development)
+                .headers(headers -> headers
+                        .contentSecurityPolicy(csp -> csp
+                                .policyDirectives("form-action 'self' http://localhost:7051")
+                        )
+                )
                 .build();
     }
 }
