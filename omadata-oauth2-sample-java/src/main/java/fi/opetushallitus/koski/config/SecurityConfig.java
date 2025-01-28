41fce85a9f4d1d9b5e6cc67370b3e3ad95fc99f9
NhutDo <nhut.do@elenium.fi>
2025-01-28 12:12:01 +0200

    OPHJOD-1217 Remove oauth2Login, use oauth2Client as it won't authenticate the user but will seek permission from the OAuth2 authorization server for the resources (scopes) it needs to access.

--- omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/config/SecurityConfig.java
+++ omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/config/SecurityConfig.java
@@ -1,16 +1,9 @@
 package fi.opetushallitus.koski.config;
 
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-
-import java.io.IOException;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -35,23 +28,7 @@ public class SecurityConfig {
                                 .permitAll()
                                 .anyRequest().authenticated()
                 )
-                .oauth2Login(oauth2Login ->
-                        oauth2Login
-                                .authorizationEndpoint(authorizationEndpoint ->
-                                        authorizationEndpoint
-                                                .authorizationRequestResolver(koskiAuthorizationRequestResolver)
-                                ).successHandler(new AuthenticationSuccessHandler() {
-                                    @Override
-                                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
-                                        //Why isn't this being called?
-                                        System.out.println("Authentication success");
-                                    }
-                                }).failureHandler((request, response, exception) -> {
-                                    response.sendRedirect("/error");
-                                })
-                )
-                .oauth2Client(withDefaults())
-                .formLogin(withDefaults()) //Remove when done.
+                .oauth2Client(withDefaults()) //Trigger login with /oauth2/authorization/koski
                 .build();
     }
 }
