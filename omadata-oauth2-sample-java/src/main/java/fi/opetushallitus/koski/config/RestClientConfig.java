f7cc8ef8b781a690564e8a799fc672fc92d9d08c
NhutDo <nhut.do@elenium.fi>
2025-01-30 11:32:31 +0200

    OPHJOD-1217 Add fetch data from Resource Server using OAuth2.

--- omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/config/RestClientConfig.java
+++ omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/config/RestClientConfig.java
@@ -6,7 +6,11 @@ import org.springframework.boot.ssl.SslBundles;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
 import org.springframework.security.oauth2.core.http.converter.OAuth2AccessTokenResponseHttpMessageConverter;
 import org.springframework.web.client.RestClient;
 
@@ -15,14 +19,18 @@ import java.time.Duration;
 @Configuration
 public class RestClientConfig {
 
+    private final String SSL_BUNDLE_KOSKI = "koski-ssl-bundle";
+
     @Bean("koski")
-    public RestClient oauthRestClient(SslBundles sslBundles) {
-        return RestClient.builder()
+    public RestClient oauthRestClient(RestClient.Builder builder, OAuth2AuthorizedClientManager authorizedClientManager,
+                                      SslBundles sslBundles) {
+        return builder
+                .requestInterceptor(new OAuth2ClientHttpRequestInterceptor(authorizedClientManager))
                 .requestFactory(
                         ClientHttpRequestFactoryBuilder.jdk()
                                 .build(
                                         ClientHttpRequestFactorySettings.defaults()
-                                                .withSslBundle(sslBundles.getBundle("koski-ssl-bundle"))
+                                                .withSslBundle(sslBundles.getBundle(SSL_BUNDLE_KOSKI))
                                                 .withConnectTimeout(Duration.ofSeconds(5))
                                                 .withReadTimeout(Duration.ofSeconds(10))))
                 .messageConverters(
@@ -30,6 +38,8 @@ public class RestClientConfig {
                             messageConverters.clear();
                             messageConverters.add(new FormHttpMessageConverter());
                             messageConverters.add(new OAuth2AccessTokenResponseHttpMessageConverter());
+                            messageConverters.add(new StringHttpMessageConverter());
+                            messageConverters.add(new MappingJackson2HttpMessageConverter());
                         })
                 .defaultStatusHandler(new OAuth2ErrorResponseErrorHandler())
                 .build();
--- omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/controller/KoskiController.java
+++ omadata-oauth2-sample-java/src/main/java/fi/opetushallitus/koski/controller/KoskiController.java
@@ -2,9 +2,12 @@ package fi.opetushallitus.koski.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +15,13 @@ import org.springframework.web.client.RestClient;
 
 import java.util.Objects;
 
+import static org.springframework.security.oauth2.client.web.client.RequestAttributeClientRegistrationIdResolver.clientRegistrationId;
+
+@Slf4j
 @RestController("/")
 public class KoskiController {
 
     private final RestClient restClient;
-
     private final OAuth2AuthorizedClientRepository authorizedClientRepository;
 
     @Value("${koski.resource-server.url}")
@@ -48,6 +53,8 @@ public class KoskiController {
                             <br/>Issued at: %s
                             <br/>Expires at: %s
                             <br/>Scopes: %s
+                            <br/><br/>
+                            <h2><a href="/resource-server/fetch">Fetch data from resource server</a></h2>
                             <br/><h1><a href="/oauth2/logout/koski">Logout</a></h1>
                             """.formatted(accessToken.getTokenValue(),
                     accessToken.getIssuedAt(), accessToken.getExpiresAt(), accessToken.getScopes())
@@ -66,5 +73,31 @@ public class KoskiController {
         return """
                 <center><h1>Logged out</h1><br/><h2><a href="/">Home</a></h2></center>
                 """;
+    }
+
+    @GetMapping("/resource-server/fetch")
+    public String fetchData(Authentication authentication, HttpServletRequest request) {
+        var koski = getKoskiAuthorizedClient(authentication, request);
+        if (koski == null) {
+            return """
+                    <center><h1>You are not authenticated</h1><br/><h2><a href="/">Home</a></h2></center>
+                    """;
+        }
+
+        try {
+            return restClient.post()
+                    .uri(resourceServerUrl)
+                    .header("Authorization", "Bearer " + Objects.requireNonNull(koski.getAccessToken()).getTokenValue())
+                    .attributes(clientRegistrationId("koski"))
+                    .accept(MediaType.APPLICATION_JSON)
+                    .retrieve()
+                    .body(String.class);
+
+        } catch (Exception e) {
+            log.error("Failed to fetch data from resource server", e);
+            return """
+                    <center><h1>Failed to fetch data from resource server</h1><br/><h2><a href="/">Home</a></h2></center>
+                    """;
+        }
     }
 }
